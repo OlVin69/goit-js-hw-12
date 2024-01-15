29db1949fa6de95a384b0045af340cfff6902567
OlVin69 <aviniichuk@gmail.com>
2024-01-15 09:57:25 +0200

    update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from "axios";
 
 
 const formEl = document.querySelector(".form");
